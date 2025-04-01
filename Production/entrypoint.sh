febd5c47ee57f7732caa8da8e824457197990615
i.khurani <huran438@gmail.com>
2025-04-01 23:23:34 +0300

    updated nginx

--- Production/entrypoint.sh
+++ Production/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# Replace variables and start nginx
-envsubst '$API_URL $DASHBOARD_URL' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+# Make sure template is rendered with environment variables
+envsubst '${API_URL} ${DASHBOARD_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
+# Start Nginx
 exec nginx -g 'daemon off;'
--- Production/nginx_entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Replace variables and start nginx
-envsubst '$SERVER_NAME $BACKEND_URL' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
-
-exec nginx -g 'daemon off;'
