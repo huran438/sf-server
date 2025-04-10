45c8d65445403ff77b6fd8059413af539ebabe1c
i.khurani <huran438@gmail.com>
2025-04-10 22:56:14 +0300

    updated dashboard login

--- SFServer.Shared/Server/Auth/DashboardLoginRequest.cs
+++ SFServer.Shared/Server/Auth/DashboardLoginRequest.cs
@@ -1,14 +1,9 @@
-﻿using MessagePack;
-
-namespace SFServer.Shared.Server.Auth
+﻿namespace SFServer.Shared.Server.Auth
 {
-    [MessagePackObject]
     public class LoginDashboardRequest
     {
-        [Key(0)]
         public string Credential { get; set; }
-
-        [Key(1)]
+        
         public string Password { get; set; }
     }
 }
