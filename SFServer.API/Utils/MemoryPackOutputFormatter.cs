f11d7700c4ddca5a82aa110dcf51386e093b5b3c
i.khurani <huran438@gmail.com>
2025-06-16 14:40:11 +0300

    revert

--- SFServer.API/Utils/MemoryPackOutputFormatter.cs
+++ SFServer.API/Utils/MemoryPackOutputFormatter.cs
@@ -21,9 +21,7 @@ public class MemoryPackOutputFormatter : OutputFormatter
     {
         var httpContext = context.HttpContext;
         httpContext.Response.ContentType = "application/x-memorypack";
-        if (context.Object is null) return;
-        var objectType = context.ObjectType ?? context.Object.GetType() ?? typeof(object);
-        var buffer = MemoryPackSerializer.Serialize(objectType, context.Object);
+        var buffer = MemoryPackSerializer.Serialize(context.ObjectType!, context.Object);
         await using var brotliStream = new BrotliStream(httpContext.Response.Body, CompressionMode.Compress, leaveOpen: true);
         await brotliStream.WriteAsync(buffer);
     }
