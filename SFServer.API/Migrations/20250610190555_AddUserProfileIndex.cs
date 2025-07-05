8906932566cc1869a0b07774bcacc91ae980da2e
i.khurani <huran438@gmail.com>
2025-07-05 09:48:53 +0300

    Added lost migration

---
+++ SFServer.API/Migrations/20250610190555_AddUserProfileIndex.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SFServer.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserProfileIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateSequence<int>(
+                name: "UserProfileIndex",
+                schema: "dbo");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Index",
+                table: "UserProfiles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Index",
+                table: "UserProfiles");
+
+            migrationBuilder.DropSequence(
+                name: "UserProfileIndex",
+                schema: "dbo");
+        }
+    }
+}
